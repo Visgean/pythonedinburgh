c0fcd93ec6ddc9a7ded1047b5a5e06fac9d9a726
Visgean Skeloru <me@visgean.me>
2017-02-18 16:57:45 +0000

    fix tests for context

--- pew/main/tests.py
+++ pew/main/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from datetime import timedelta
 from django.test.client import RequestFactory
+from django.test import Client
 from django.utils import timezone
 
 from events.models import Event
@@ -12,6 +13,7 @@ class HomeViewTestCase(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
+        self.client = Client()
 
         self.events = [
             Event.objects.create(
@@ -25,13 +27,13 @@ class HomeViewTestCase(TestCase):
         ]
 
     def test_home_context_dict(self):
-        under_test = views._home_context_dict()
+        response = self.client.get('/')
         self.assertSequenceEqual(
-            under_test['upcoming_events'],
+            response.context['upcoming_events'],
             self.events
         )
 
     def test_home_dict_contains_upcoming_events(self):
         request = self.factory.get('/')
-
-        views.home(request)
+        response = views.home(request)
+        self.assertEqual(response.status_code, 200)
